b616213bcd64311f05b27a90f4b2efe86a10c65f
Islam Solaiman <islam.solaiman92@gmail.com>
2024-07-26 16:48:45 +0400

    Omdb Api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 // import App from "./App";
+import App1 from "./App1";
 import StarRating from "./starRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App />*/}
-    <StarRating
+    <App1 />
+    {/* <StarRating
       maxRating={5}
       defaultRating={3}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
 
-    <StarRating size={24} maxRating={5} color="red" className="test" defaultRating={3} />
+    <StarRating size={24} maxRating={5} color="red" className="test" defaultRating={3} /> */}
   </React.StrictMode>
 );
