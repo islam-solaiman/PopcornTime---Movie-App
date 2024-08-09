9f989fc263befb153a0dd3a67e28e845d6e22a12
Islam Solaiman <islam.solaiman92@gmail.com>
2024-08-09 13:03:35 +0400

    Added Local Storage

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import App1 from "./App1";
+// import App1 from "./App1";
+import App2 from './App2'
 import StarRating from "./starRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App1 />
+    <App2 />
     {/* <StarRating
       maxRating={5}
       defaultRating={3}
